a66315b254729844011e8677dad12e4efd3c6010
Mohamed Agamy <mohamedagamy327@gmail.com>
2021-06-02 13:47:00 +0200

    bin

--- src/app/components/home/features/bin-variables/bin-variables.component.ts
+++ src/app/components/home/features/bin-variables/bin-variables.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PageTitleService, CoreService, PlaceHolderService } from 'src/app/core/services';
 
 @Component({
   selector: 'app-bin-variables',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BinVariablesComponent implements OnInit {
 
-  constructor() { }
+  tradeHistory: any;
+  coinList: any;
+  tradeHistoryColumns = ['currency', 'txnno', 'status', 'price', 'total', 'date', 'from', 'more'];
+  cryptoCompareColumns = ['serial_number', 'desktop_name', 'mobile_name', 'price', 'volume', 'tag', 'total_volume', 'market_cap', 'circulating_supply', 'chart', 'change'];
+
+
+  // Exchange Statistics
+  statisticConfig = { slidesToShow: 1, fade: true, slidesToScroll: 1, arrows: false, dots: false, autoplay: true, autoplaySpeed: 2000 };
+
+
+
+  constructor(private pageTitleService: PageTitleService,
+    private translate: TranslateService,
+    private service: CoreService,
+    private placeHolderService: PlaceHolderService) { }
 
   ngOnInit(): void {
+    this.pageTitleService.setTitle('Bin Variables');
+
+    this.placeHolderService.getTradeHistoryContent().
+      subscribe(res => { this.tradeHistory = res },
+        err => console.log(err),
+        () => this.tradeHistory
+      );
+
+    this.placeHolderService.getCoinList().
+      subscribe(res => { this.coinList = res },
+        err => console.log(err)
+      );
   }
+
+
+
 
 }
