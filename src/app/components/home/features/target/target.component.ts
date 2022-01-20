1a97a402641f9059e292069feddcd73ebc750b10
Mohamed Agamy <mohamedagamy327@gmail.com>
2022-01-21 00:54:08 +0200

    target page

--- src/app/components/home/features/target/target.component.ts
+++ src/app/components/home/features/target/target.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { FileValidators } from 'ngx-file-drag-drop';
 import { PageTitleService, UploadService } from 'src/app/core/services';
 
@@ -13,9 +15,14 @@ export class TargetComponent implements OnInit {
   fileFormData: FormData;
   columns: Array<any>;
   displayedColumns: Array<any>;
-  dataSource: any;
+  date: any;
+  @ViewChild('paginator') paginator: MatPaginator;
+
+  columnsList: string[];
 
   public chooseColumnCtrl: FormControl = new FormControl();
+
+  dataSource: MatTableDataSource<any>;
 
   constructor(
     private pageTitleService: PageTitleService,
@@ -33,16 +40,17 @@ export class TargetComponent implements OnInit {
     this.chooseColumnCtrl.valueChanges.subscribe((value) => {
       if (value) {
 
+        if (!this.displayedColumns.find(d => d == 'probability')) {
+          this.date = this.date.map(obj => ({ ...obj, probability: null }));
 
-        this.dataSource = this.dataSource.map(obj => ({ ...obj, probability: null }));
+          this.columns.unshift({
+            columnDef: 'probability',
+            header: 'probability',
+            cell: (element: any) => `${element['probability'] ? element['probability'] : ``}`
+          });
 
-        this.columns.unshift({
-          columnDef: 'probability',
-          header: 'probability',
-          cell: (element: any) => `${element['probability'] ? element['probability'] : ``}`
-        });
-
-        this.displayedColumns = this.columns.map(c => c.columnDef);
+          this.displayedColumns = this.columns.map(c => c.columnDef);
+        }
 
         const index = this.displayedColumns.findIndex(d => d == value);
         this.displayedColumns.splice(index, 1);
@@ -72,6 +80,7 @@ export class TargetComponent implements OnInit {
   upload(file: any) {
     this.uploadService.upload(file).subscribe(
       (res: any) => {
+        this.date = res;
         const columns = res
           .reduce((columns: any, row: any) => {
             return [...columns, ...Object.keys(row)]
@@ -89,7 +98,9 @@ export class TargetComponent implements OnInit {
           }
         })
         this.displayedColumns = this.columns.map(c => c.columnDef);
-        this.dataSource = res.slice(0, 100);
+        this.columnsList = this.columns.map(c => c.columnDef);
+        this.dataSource = new MatTableDataSource(this.date);
+        this.dataSource.paginator = this.paginator;
       });
   }
 
